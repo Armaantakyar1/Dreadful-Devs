c0062c278fd2ecd38b2cdab7e308275d5e38ec3e
Armaantakyar1 <102411631+Armaantakyar1@users.noreply.github.com>
2023-04-07 17:14:19 +0400

    joystck fix

--- Assets/Scripts/Ahmed/PlayerMovementController.cs
+++ Assets/Scripts/Ahmed/PlayerMovementController.cs
@@ -15,6 +15,7 @@ public class PlayerMovementController : MonoBehaviour
     float yStore;
 
     [Header("Joystick Mapping")]
+
     [SerializeField] string rightStickHorizontal;
     [SerializeField] string rightStickVertical;
 
